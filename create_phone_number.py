f8cb7c53576b10d54d8fc302a040cc196228b9bf
Auca <forosypaginas@gmail.com>
2022-09-24 09:21:52 -0300

    adjusted pre-commit

--- create_phone_number.py
+++ create_phone_number.py
@@ -6,17 +6,15 @@ create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890
 The returned format must be correct in order to complete this challenge.
 
 Don't forget the space after the closing parentheses! """
-
-from typing import List
 import re
+from typing import List
+
 
 def create_phone_number(input_ints: List[int]) -> str:
     # convert list into str
-    number_list = ''.join(str(number) for number in input_ints)
-    phone = re.sub(r'(\d{3})(\d{3})(\d{4})', r'(\1) \2-\3', number_list)
+    number_list = "".join(str(number) for number in input_ints)
+    phone = re.sub(r"(\d{3})(\d{3})(\d{4})", r"(\1) \2-\3", number_list)
     return phone
-
-
 
 
 input = create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
